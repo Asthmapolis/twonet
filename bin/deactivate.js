2de26081243f81d9628c7e6ab6ce23137918e7be
Greg Tracy <gtracy@gmail.com>
2017-05-31 16:57:07 -0500

    adding more polish to the CLI options

--- bin/deactivate.js
+++ bin/deactivate.js
@@ -1,21 +1,55 @@
 var async = require('async');
+var fs = require('fs');
 var TwoNetAPI = require('../lib/twonet');
 var config = require('../lib/config');
 
-// default to sandbox environment
-var env = 'sandbox';
-if( process.argv[2] == 'production' ) {
-	env = 'production';
+//
+// Command use : node bin/deactivate.js <hub-file> <env>
+//   <hub-file> is a text file containing hub IDs on each line
+//   <env> optional environment declaration. defaults to production
+//
+function print_usage() {
+	console.log("\nUsage : \n");
+	console.log("node bin/deactivate.js <hub-file> <env>");
+	console.log("    <hub-file> is a text file containing hub IDs on each line");
+	console.log("    <env> optional environment declaration - production/sandbox. defaults to production");
+	console.log("\n");
 }
+
+if( process.argv.length < 3 || process.argv[2].toLowerCase().indexOf('help') >= 0 ) {
+	print_usage();
+	process.exit(1);
+} else {
+	// default to production environment
+	var env = 'production';
+	if( process.argv.length === 4 ) {
+		var argv_env = process.argv[3];
+		if( argv_env === 'sandbox' ) {
+			env = 'sandbox';
+		} else if( argv_env !== 'production' ) {
+			console.log("\nHmph. I don't recognize that environment, " + argv_env);
+			print_usage();
+			process.exit(1);
+		}
+	}
+	var filename = process.argv[2];
+	try {
+        var hub_list = fs.readFileSync(filename).toString().split("\n");
+    } catch( exception ) {
+    	if( exception.code === 'ENOENT' ) {
+            console.log('\nUnable to find and open, ' + exception.path);
+    	} else {
+    		console.dir(exception);
+    	}
+    	print_usage();
+    	process.exit(1);
+    }
+}
+
+console.log('\n*** You are about to deactivate ' + hub_list.length + ' hubs *** ');
+console.log('\nAre you sure you want to do this? Y/n  ');
+
 var twoNetApi = new TwoNetAPI(config.customer_id, config[env].auth_key, env);
-
-console.log('Deactivate a list of hubs on ' + env);
-console.log('... from ' + process.argv[3]);
-
-var fs = require('fs');
-var hub_list = fs.readFileSync(process.argv[3]).toString().split("\n");
-
-console.log('Deactivating...');
 async.eachSeries(hub_list,function(hub_id,cb) {
 	console.log('\t'+hub_id);
 	twoNetApi.deactivateHub(hub_id, function(status, result) {
--- examples/constants.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	customer_id : 'FIXME',
-	sandbox : {
-        auth_key : 'FIXME'
-    },
-    production : {
-        auth_key : 'FIXME'
-    }
-}
